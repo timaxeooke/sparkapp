08ef3fcd441edf0c5f631587b862dcfb285d4bf0
Theo Lipeles <Theo@Lipeles.com>
2017-11-14 17:31:08 -0500

    adds jsdoc comments to Firebase Class

--- Firebase.js
+++ Firebase.js
@@ -1,14 +1,16 @@
-import *  as firebase from 'firebase';
+import * as firebase from 'firebase';
 
+/** A wrapper for Firebase */
 class Firebase {
-  static initialize () {
+  /** Initializes Firebase */
+  static initialize() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBAQ4UHMg79qqGExk2aPUnR6_SAHRqc2SM',
       authDomain: 'sparkapp-9d602.firebaseapp.com',
       databaseURL: 'https://sparkapp-9d602.firebaseio.com',
       projectId: 'sparkapp-9d602',
       storageBucket: 'sparkapp-9d602.appspot.com',
-      messagingSenderId: '213231738935'
+      messagingSenderId: '213231738935',
     });
   }
 }
